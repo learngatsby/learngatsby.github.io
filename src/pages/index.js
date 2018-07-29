f882247a42088a30da4a32211cb106397763f9e1
Luan Orlandi <orlandi.luan@gmail.com>
2018-07-29 16:20:52 -0300

    Implement Hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,36 @@
 import React from 'react';
 import Link from 'gatsby-link';
 
+import profileImage from '../assets/luan.jpg';
+
 const IndexPage = () => (
   <div>
-    <h1>
-      Hi people
-    </h1>
-    <p>
-      Welcome to your new Gatsby site.
-    </p>
-    <p>
-      Now go build something great.
-    </p>
-    <Link to="/page-2/">
-      Go to page 2
-    </Link>
+    <section className="section container">
+      <div className="columns">
+        <div className="column level">
+          <div className="level-item">
+            <figure className="image is-128x128">
+              <img
+                className="is-rounded"
+                src={profileImage}
+                alt="Luan Orlandi Profile"
+              />
+            </figure>
+          </div>
+          <h1 className="title level-item">
+            Luan Orlandi
+          </h1>
+          <h2 className="subtitle level-item">
+            Web Developer
+          </h2>
+        </div>
+      </div>
+      <div className="container">
+        <Link to="/page-2/">
+          Go to page 2
+        </Link>
+      </div>
+    </section>
   </div>
 );
 
