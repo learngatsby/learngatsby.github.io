a860c2c2eb17c1c14a1a26f21be1276e3d6881f7
Luan Orlandi <orlandi.luan@gmail.com>
2018-07-29 17:15:21 -0300

    Add social media links

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,26 +3,48 @@ import Link from 'gatsby-link';
 
 import profileImage from '../assets/luan.jpg';
 
+const socialMedias = [
+  { icon: 'fa-github', link: 'https://github.com/luanorlandi' },
+  { icon: 'fa-twitter', link: 'https://twitter.com/luanorlandi' },
+  { icon: 'fa-medium', link: 'https://medium.com/@luanorlandi' },
+  { icon: 'fa-linkedin', link: 'https://www.linkedin.com/in/luanorlandi/' },
+];
+
 const IndexPage = () => (
   <div>
-    <section className="section container">
-      <div className="columns">
-        <div className="column level">
-          <div className="level-item">
-            <figure className="image is-128x128">
-              <img
-                className="is-rounded"
-                src={profileImage}
-                alt="Luan Orlandi Profile"
-              />
-            </figure>
+    <section className="section">
+      <div className="container">
+        <div className="columns is-mobile">
+          <div className="column level">
+            <div className="level-item">
+              <figure className="image is-128x128">
+                <img
+                  className="is-rounded"
+                  src={profileImage}
+                  alt="Luan Orlandi Profile"
+                />
+              </figure>
+            </div>
+            <h1 className="title has-text-centered">
+              Luan Orlandi
+            </h1>
+            <h2 className="subtitle has-text-centered">
+              Web Developer
+            </h2>
           </div>
-          <h1 className="title level-item">
-            Luan Orlandi
-          </h1>
-          <h2 className="subtitle level-item">
-            Web Developer
-          </h2>
+        </div>
+      </div>
+      <div className="container">
+        <div className="columns is-mobile is-centered">
+          {socialMedias.map(socialMedia => (
+            <div className="column is-1">
+              <a href={socialMedia.link}>
+                <span className="icon is-large">
+                  <i className={`fab fa-2x ${socialMedia.icon}`} />
+                </span>
+              </a>
+            </div>
+          ))}
         </div>
       </div>
       <div className="container">
