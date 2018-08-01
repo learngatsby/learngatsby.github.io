4a299c5677a2e587981f50daeb6258af0a4277e9
Luan Orlandi <orlandi.luan@gmail.com>
2018-07-31 23:17:01 -0300

    Create Posts page

---
+++ src/pages/posts.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import Card from '../components/Card';
+import postImage from '../assets/learn-react.jpg';
+
+const projectCards = [
+  {
+    title: 'Learn React easily with this rule',
+    subtitle: 'Make it fun',
+    link: 'https://medium.com/@luanorlandi/learn-react-easily-with-this-rule-a715b818a358',
+    imageLink: postImage,
+    tags: ['React', 'Aprender'],
+  },
+];
+
+const Posts = () => (
+  <section className="section">
+    <h1 className="title has-text-centered">
+      Posts
+    </h1>
+    <div className="container">
+      <hr />
+      <div className="columns">
+        <div className="column is-half is-offset-one-quarter">
+          {projectCards.map(projectCard => (
+            <Card {...projectCard} />
+          ))}
+        </div>
+      </div>
+    </div>
+  </section>
+);
+
+export default Posts;
