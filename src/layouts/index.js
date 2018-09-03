c284a302ff897202085e9c01e471e6b2e1741d3d
luanorlandi <orlandi.luan@gmail.com>
2018-09-03 03:15:46 -0300

    Create Header

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 
-import Header from '../components/header';
+import Header from '../components/Header';
 import '../styles/index.scss';
 
 const Layout = ({ children, data }) => (
