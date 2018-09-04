b8493eb9f98910673b10208027c37a40482e9e69
luanorlandi <orlandi.luan@gmail.com>
2018-09-04 01:55:37 -0300

    Use Card component on posts

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
+import Card from '../components/Card';
+import postImage from '../assets/learn-react.jpg';
+
+const projectCards = [
+  {
+    title: 'Learn React easily with this rule',
+    subtitle: 'Make it fun',
+    link: 'https://medium.com/@luanorlandi/learn-react-easily-with-this-rule-a715b818a358',
+    imageLink: postImage,
+    tags: ['React'],
+  },
+];
+
 const Posts = () => (
   <section className="section is-size-4-desktop is-size-5-touch">
     <h1 className="title has-text-centered">
@@ -8,7 +21,11 @@ const Posts = () => (
     <div className="container">
       <hr />
       <div className="columns">
-        <div className="column is-half is-offset-one-quarter" />
+        <div className="column is-half is-offset-one-quarter">
+          {projectCards.map(projectCard => (
+            <Card key={projectCard.title} {...projectCard} />
+          ))}
+        </div>
       </div>
     </div>
   </section>
