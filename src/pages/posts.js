7814ae02696e0954b643162fe3338ecab381cdb2
luanorlandi <orlandi.luan@gmail.com>
2018-09-04 01:52:33 -0300

    Create posts page

---
+++ src/pages/posts.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Posts = () => (
+  <section className="section is-size-4-desktop is-size-5-touch">
+    <h1 className="title has-text-centered">
+      Posts
+    </h1>
+    <div className="container">
+      <hr />
+      <div className="columns">
+        <div className="column is-half is-offset-one-quarter" />
+      </div>
+    </div>
+  </section>
+);
+
+export default Posts;
