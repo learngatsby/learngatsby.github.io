b1bdfee107edf6069d8af3f5a202fd4c86fc02d6
luanorlandi <orlandi.luan@gmail.com>
2018-09-08 01:44:07 -0300

    Create hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import Link from 'gatsby-link';
 
+import profileImage from '../assets/profile.jpg';
+
 const IndexPage = () => (
   <div>
+    <div className="container">
+      <figure className="photo image is-128x128">
+        <img
+          className="is-rounded"
+          src={profileImage}
+          alt="Luan Orlandi portfolio"
+        />
+      </figure>
+      <h1 className="title has-text-centered">
+        Luan Orlandi
+      </h1>
+      <h2 className="subtitle has-text-centered">
+        Desenvolvedor Web
+      </h2>
+    </div>
     <section className="section is-size-4-desktop is-size-5-touch">
       <div className="container content">
         <h4 className="title">
@@ -13,7 +30,7 @@ const IndexPage = () => (
           </span>
         </h4>
         <p>
-          Sou desenvolverdor Web, trabalho com Front-end criando aplicações
+          Sou desenvolvedor Web, trabalho com Front-end criando aplicações
           e páginas Web.
           Participo de eventos de tecnologia e gosto de estudar produtividade e
           comunicação, que me ajuda a manter organizado e colaborativo.
