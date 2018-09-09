0c268496baaca2c120de49d0c4c8c6ca9d7f63a1
luanorlandi <orlandi.luan@gmail.com>
2018-09-09 00:04:12 -0300

    Add post to page

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
+import Card from '../components/Card';
+import postImage from '../assets/learn-react.jpg';
+
+const postCards = [
+  {
+    title: 'Learn React easily with this rule',
+    subtitle: 'Make it fun',
+    link: 'https://medium.com/@luanorlandi/learn-react-easily-with-this-rule-a715b818a358',
+    imageLink: postImage,
+    tags: ['React'],
+  },
+];
+
 const Posts = () => (
   <section className="section">
     <h1 className="title has-text-centered">
@@ -8,7 +21,11 @@ const Posts = () => (
     <div className="container">
       <hr />
       <div className="columns is-centered">
-        <div className="column is-half" />
+        <div className="column is-half">
+          {postCards.map(postCard => (
+            <Card key={postCard.title} {...postCard} />
+          ))}
+        </div>
       </div>
     </div>
   </section>
