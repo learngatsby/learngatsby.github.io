eb8c140fd5b4a37c36bbf6eb5a117af4e224226a
luanorlandi <orlandi.luan@gmail.com>
2018-09-08 23:51:51 -0300

    Create posts

---
+++ src/pages/posts.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Posts = () => (
+  <section className="section">
+    <h1 className="title has-text-centered">
+      Posts
+    </h1>
+    <div className="container">
+      <hr />
+      <div className="columns is-centered">
+        <div className="column is-half" />
+      </div>
+    </div>
+  </section>
+);
+
+export default Posts;
