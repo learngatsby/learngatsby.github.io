c3306e031eca8fe84fa9503a8f407d82b014f619
luanorlandi <orlandi.luan@gmail.com>
2018-09-09 00:42:27 -0300

    Create stack page

---
+++ src/pages/stack.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Stack = () => (
+  <section className="section is-size-5-desktop is-size-6-touch">
+    <h1 className="title has-text-centered">
+      Stack
+    </h1>
+    <div className="container has-text-centered">
+      <hr />
+      <p className="content">
+        Tenho experiência com diversas ferramentas e tecnologias modernas,
+        que uso dependendo projeto.
+      </p>
+      <p className="content">
+        Compartilho minha stack no StackShare:
+      </p>
+      <div className="columns is-centered">
+        <div className="column is-half" />
+      </div>
+    </div>
+  </section>
+);
+
+export default Stack;
