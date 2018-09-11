ae96d13aab33f2d83ca8701dc333a5e6b5d34651
luanorlandi <orlandi.luan@gmail.com>
2018-09-11 00:05:24 -0300

    Add tracking services

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,26 @@ const pathPrefix = '/gatsby-course';
 module.exports = {
   pathPrefix,
   siteMetadata: {
-    title: 'Luan Orlandi',
+    title: process.env.GOOGLE_ANALYTICS_GATSBY_COURSE,
     url: `https://luanorlandi.github.io${pathPrefix}`,
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_GATSBY_COURSE,
+        head: true,
+        anonymize: true,
+        respectDNT: true,
+      },
+    }, {
+      resolve: 'gatsby-plugin-hotjar',
+      options: {
+        id: process.env.HOTJAR_ID_GATSBY_COURSE,
+        sv: process.env.HOTJAR_SNIPPET_VERSION_GATSBY_COURSE,
+      },
+    },
   ],
 };
