e589b0a2f0133257d69bd67bdb578228ef378855
Luan Orlandi <orlandi.luan@gmail.com>
2018-09-24 01:30:48 -0300

    Update meta tags

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,6 +5,7 @@ import { StaticQuery, graphql } from 'gatsby';
 
 import Header from './header';
 import '../styles/index.scss';
+import ogImage from '../assets/meta/luanorlandi.jpg';
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -19,20 +20,35 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            {
-              name: 'description',
-              content: 'Desenvolvedor Front-end, criando apps e páginas Web',
-            },
-            {
-              name: 'keywords',
-              content: 'React, Node, Gatsby, Front-end, desenvolvedor, API',
-            },
-          ]}
-        >
+        <Helmet>
           <html lang="pt" />
+          <title>{data.site.siteMetadata.title}</title>
+          <meta
+            name="description"
+            content="Desenvolvedor Front-end, criando apps e páginas Web"
+          />
+          <meta
+            name="keywords"
+            content="React, Node, Gatsby, Front-end, desenvolvedor, API"
+          />
+          <meta
+            property="og:url"
+            content="https://luanorlandi.github.io/gatsby-course"
+          />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content={data.site.siteMetadata.title} />
+          <meta
+            property="og:description"
+            content="Desenvolvedor Front-end, criando apps e páginas Web"
+          />
+          <meta property="og:image" content={ogImage} />
+          <meta
+            property="og:alt"
+            content="Desenvolvedor Front-end, criando apps e páginas Web"
+          />
+          <meta property="og:type" content="image/jpg" />
+          <meta property="og:width" content="1365" />
+          <meta property="og:height" content="1365" />
         </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
         {children}
