9ade5a234ccb7b4f2e84b3c2a2581d4b99ac262a
Luan Orlandi <orlandi.luan@gmail.com>
2018-09-24 00:03:59 -0300

    Create post page

---
+++ src/pages/posts.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+
+const Posts = () => (
+  <Layout>
+    <section className="section">
+      <h1 className="title has-text-centered">Posts</h1>
+      <div className="container">
+        <hr />
+        <div className="columns is-centered">
+          <div className="column is-half" />
+        </div>
+      </div>
+    </section>
+  </Layout>
+);
+
+export default Posts;
