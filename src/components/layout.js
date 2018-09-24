265a72297a46877ddbd5b5254cde72afbb85e002
Luan Orlandi <orlandi.luan@gmail.com>
2018-09-24 19:23:43 -0300

    Create summary

--- src/components/layout.js
+++ src/components/layout.js
@@ -22,23 +22,20 @@ const Layout = ({ children }) => (
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
+            {
+              name: 'description',
+              content: 'Desenvolvedor Front-end, criando apps e páginas Web',
+            },
+            {
+              name: 'keywords',
+              content: 'React, Node, Front-end, desenvolvedor, API',
+            },
           ]}
         >
           <html lang="en" />
         </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
+        {children}
       </>
     )}
   />
