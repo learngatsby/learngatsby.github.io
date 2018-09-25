91ab3dc2788ae53e43dbef467a261fb7b65c5924
Luan Orlandi <orlandi.luan@gmail.com>
2018-09-24 21:22:40 -0300

    Create hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
+import profileImage from '../assets/profile.jpg';
 
 const IndexPage = () => (
   <Layout>
+    <div className="container">
+      <figure className="photo image is-128x128">
+        <img className="is-rounded" src={profileImage} alt="Luan Orlandi" />
+      </figure>
+      <h1 className="title has-text-centered">Luan Orlandi</h1>
+      <h2 className="subtitle has-text-centered">Desenvolvedor Web</h2>
+    </div>
     <section className="section is-size-4 is-size-5-touch">
       <div className="container content">
         <h4 className="title">
