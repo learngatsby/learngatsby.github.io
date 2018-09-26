7191e424b4277c6cc18670477e5116ec8055bd26
Luan Orlandi <orlandi.luan@gmail.com>
2018-09-25 22:27:41 -0300

    Create projects page

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -2,12 +2,49 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
+import projectImage from '../assets/tic-tac-porg.jpg';
 
 const SecondPage = () => (
   <Layout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <section className="section is-size-5-desktop is-size-6-touch">
+      <div className="container">
+        <h1 className="title has-text-centered">Projetos</h1>
+        <hr />
+        <p className="content has-text-centered">
+          Alguns dos meus projetos de destaque, veja mais no meu{' '}
+          <a href="https://github.com/luanorlandi">GitHub</a>.
+        </p>
+      </div>
+      <div className="columns is-centered">
+        <div className="column is-half">
+          <a href="https://luanorlandi.github.io/tic-tac-porg">
+            <div className="card">
+              <div className="card-content">
+                <div className="media">
+                  <div className="media-left">
+                    <figure className="image is-96x96">
+                      <img src={projectImage} alt="Tic-tac-porg" />
+                    </figure>
+                  </div>
+                  <div className="media-content">
+                    <p className="title is-size-5-desktop is-size-6-touch">
+                      Tic-tac-porg
+                    </p>
+                    <p className="subtitle is-size-5-desktop is-size-6-touch">
+                      Jogo da Velha com tema de Star Wars
+                    </p>
+                    <div className="tags">
+                      <span className="tag">React</span>
+                      <span className="tag">PWA</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </section>
   </Layout>
 );
 
