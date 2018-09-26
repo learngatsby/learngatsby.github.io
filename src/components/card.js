9a16e747afe8b23773dacb584280d4d18be9e670
Luan Orlandi <orlandi.luan@gmail.com>
2018-09-25 22:51:07 -0300

    Create card component

---
+++ src/components/card.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Tags } from 'opentracing';
+
+const Card = ({ title, subtitle, link, imageLink, tags }) => (
+  <a href={link}>
+    <div className="card">
+      <div className="card-content">
+        <div className="media">
+          <div className="media-left">
+            <figure className="image is-96x96">
+              <img src={imageLink} alt={title} />
+            </figure>
+          </div>
+          <div className="media-content">
+            <p className="title is-size-5-desktop is-size-6-touch">{title}</p>
+            <p className="subtitle is-size-5-desktop is-size-6-touch">
+              {subtitle}
+            </p>
+            <div className="tags">
+              {tags.map(tag => (
+                <span className="tag" key={tag}>
+                  {tag}
+                </span>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </a>
+);
+
+export default Card;
